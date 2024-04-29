7085b1c81cc972e34af385ee62a4e52d6cfa9f54
Documenter.jl <documenter@juliadocs.github.io>
2024-04-29 12:57:06 +0000

    build based on 73ecfa7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MultivariateExpansions","category":"page"},{"location":"#MultivariateExpansions","page":"Home","title":"MultivariateExpansions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MultivariateExpansions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MultivariateExpansions]","category":"page"}]
+}
